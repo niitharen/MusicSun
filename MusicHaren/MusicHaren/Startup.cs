e465dbd954932d7dedd9c5fd01d1f3cd9b6edd9f
USER-PC <USER-PC@USERMIC-TEBA7NR>
2016-07-31 21:00:15 +0800

    新建项目

---
+++ MusicHaren/MusicHaren/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicHaren.Startup))]
+namespace MusicHaren
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
