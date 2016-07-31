4b623bce6b18b1734f01fe9611732fe15773fce0
USER-PC <USER-PC@USERMIC-TEBA7NR>
2016-07-31 22:06:04 +0800

    新建项目

---
+++ MusicShf/MusicShf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicShf.Startup))]
+namespace MusicShf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
